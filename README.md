49c07d6f26471374c7f7ebce042e648f3ecc0727
pavroo <pavroo@onet.eu>
2017-09-03 22:02:14 +0200

    sparky-backup-sys added

---
+++ README.md
@@ -0,0 +1,51 @@
+Sparky Backup System
+
+This tool lets you create a hybrid ISO image from your SparkyLinux installation. The package provides a YAD-based GUI for the sparky-backup-core.
+
+Copyright (C) 2012-2017 Paweł "pavroo" Pijanowski <pavroo@onet.eu>
+    Under GNU GPLv3
+
+The tool is forked from Remastersys & Remastersys-GUI
+Oryginaly created by Tony Brijeski under GNU GPLv2
+Copyright (C) 2007,2008,2009
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Dependencies:
+---------------
+apt
+bash
+coreutils
+gawk
+gparted
+grep
+sparky-backup-core
+sparky-editor
+sparky-remsu
+sparky-xterm
+yad
+
+Conflicts:
+--------------
+sparky-remastersys
+
+Install:
+-------------
+su (or sudo) 
+./install.sh
+
+Uninstall:
+-------------
+su (or sudo)
+./install.sh uninstall
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
+if [ "$1" = "uninstall" ]; then
+   	rm -f /usr/bin/sparkybackup-gui
+	rm -f /usr/bin/sparkybackup-skelcopy
+	rm -f /usr/share/applications/sparkybackup-gui.desktop
+	rm -f /usr/share/menu/sparky-backup-sys
+	rm -f /usr/lib/sparkycenter/system/sparkybackup-gui.desktop
+else
+	cp bin/sparkybackup-gui /usr/bin/sparkybackup-gui
+	cp bin/sparkybackup-skelcopy /usr/bin/sparkybackup-skelcopy
+	cp share/sparkybackup-gui.desktop /usr/share/applications/sparkybackup-gui.desktop
+	cp share/sparky-backup-sys /usr/share/menu/sparky-backup-sys
+	if [ ! -d /usr/lib/sparkycenter/system ]; then
+		mkdir -p /usr/lib/sparkycenter/system
+	fi
+	cp system/sparkybackup-gui.desktop /usr/lib/sparkycenter/system/sparkybackup-gui.desktop
+fi
