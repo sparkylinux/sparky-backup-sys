0cd39b936039df4810ef2ecb92d3f8fd6e1d4ea8
pavroo <pavroo@onet.eu>
2019-11-25 21:55:40 +0100

    20191125

--- install.sh
+++ install.sh
@@ -16,20 +16,18 @@
 
 if [ "$1" = "uninstall" ]; then
    	rm -f /usr/bin/sparkybackup-gui
-	#rm -f /usr/bin/sparkybackup-skelcopy
 	rm -f /usr/share/applications/sparkybackup-gui.desktop
 	rm -f /usr/share/menu/sparky-backup-sys
 	rm -f /usr/lib/sparkycenter/system/sparkybackup-gui.desktop
 	rm -rf /usr/share/sparky/sparky-backup-sys
 else
 	cp bin/sparkybackup-gui /usr/bin/sparkybackup-gui
-	#cp bin/sparkybackup-skelcopy /usr/bin/sparkybackup-skelcopy
 	cp share/sparkybackup-gui.desktop /usr/share/applications/sparkybackup-gui.desktop
 	cp share/sparky-backup-sys /usr/share/menu/sparky-backup-sys
 	if [ ! -d /usr/lib/sparkycenter/system ]; then
 		mkdir -p /usr/lib/sparkycenter/system
 	fi
-	cp system/sparkybackup-gui.desktop /usr/lib/sparkycenter/system/sparkybackup-gui.desktop
+	cp share/sparkybackup-gui.desktop /usr/lib/sparkycenter/system/sparkybackup-gui.desktop
 	if [ ! -d /usr/share/sparky/sparky-backup-sys ]; then
 		mkdir -p /usr/share/sparky/sparky-backup-sys
 	fi
